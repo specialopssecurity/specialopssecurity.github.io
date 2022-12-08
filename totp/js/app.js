e158bc8813815bc23ce794ece921296a1b64e50e
Special Ops Security <119271914+specialopssecurity@users.noreply.github.com>
2022-12-08 15:52:50 -0600

    Add files via upload

---
+++ totp/js/app.js
@@ -0,0 +1,124 @@
+function getCurrentSeconds() {
+  return Math.round(new Date().getTime() / 1000.0);
+}
+
+function stripSpaces(str) {
+  return str.replace(/\s/g, '');
+}
+
+function truncateTo(str, digits) {
+  if (str.length <= digits) {
+    return str;
+  }
+
+  return str.slice(-digits);
+}
+
+function parseURLSearch(search) {
+  const queryParams = search.substr(1).split('&').reduce(function (q, query) {
+    const chunks = query.split('=');
+    const key = chunks[0];
+    let value = decodeURIComponent(chunks[1]);
+    value = isNaN(Number(value)) ? value : Number(value);
+    return (q[key] = value, q);
+  }, {});
+
+  return queryParams;
+}
+
+
+
+new Vue({
+  el: '#app',
+  data: {
+    secret_key: 'XXXXXSAMPLEXXXXX',
+    nickname: 'user@sample.com',
+    uri: 'otpauth://',
+    digits: 6,
+    period: 30,
+    updatingIn: 0,
+    token: null,
+    clipboardButton: null,
+  },
+
+  mounted: function () {
+    this.getKeyFromUrl();
+    this.getQueryParameters()
+    this.update();
+
+    this.intervalHandle = setInterval(this.update, 1000);
+
+    this.clipboardButton = new ClipboardJS('#clipboard-button');
+  },
+
+  destroyed: function () {
+    clearInterval(this.intervalHandle);
+  },
+
+  computed: {
+    totp: function () {
+      return new OTPAuth.TOTP({
+        algorithm: 'SHA1',
+        digits: this.digits,
+        period: this.period,
+        secret: OTPAuth.Secret.fromB32(stripSpaces(this.secret_key)),
+      });
+    }
+  },
+
+  methods: {
+    update: function () {
+      this.updatingIn = this.period - (getCurrentSeconds() % this.period);
+      this.token = truncateTo(this.totp.generate(), this.digits);
+      this.uri = 'otpauth://totp/' + encodeURIComponent(this.nickname) + '?secret=' + this.secret_key;
+      document.getElementById("qr").innerHTML = "";
+      var qrcode = new QRCode(document.getElementById("qr"), this.uri);
+    },
+
+    getKeyFromUrl: function () {
+      const key = document.location.hash.replace(/[#\/]+/, '');
+
+      if (key.length > 0) {
+        this.secret_key = key;
+      }
+    },
+    getQueryParameters: function () {
+      const queryParams = parseURLSearch(window.location.search);
+
+      if (queryParams.key) {
+        this.secret_key = queryParams.key;
+      }
+
+      if (queryParams.digits) {
+        this.digits = queryParams.digits;
+      }
+
+      if (queryParams.period) {
+        this.period = queryParams.period;
+      }
+    }
+  }
+});
+
+// var qrcode = new QRCode(document.getElementById("qr"), "otpauth://totp/nickname?secret=0293480234");
+
+
+
+
+// function update_qr() {
+//   var type = 'totp'; 
+//   var secret = $("#secret_key").val().replace(/ /g, '');
+//   var label = $("#nickname").val();
+//   var uri = `otpauth://${type}/${encodeURIComponent(label)}?secret=${secret}`;
+//   $("#qr").empty().qrcode({
+//     text: uri
+//   });
+//   $("#uri").val = uri;
+  
+//   // mark empty required input fields
+//   $("#secret_key").toggleClass("is-invalid", secret === "");
+//   $("#nickname").toggleClass("is-invalid", label === "");
+// }
+// update_qr();
+// $("select,input[type='search'],input[type='checkbox']").on("change keyup input", update_qr);
+
